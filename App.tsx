a8369c609c5554c63c7d5d973db41a5fd6dcebe9
Renato Rocha <renatob.rocha@gmail.com>
2020-10-02 00:04:59 -0300

    Init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const range = (min: number, max: number) => {
+  const res = [];
+  for (let index = min; index <= max; index++) res.push(index);
+
+  return res;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {range(65, 90).map((value) => (
+          <Text key={value.toString()} style={{ marginBottom: 10 }}>
+            {String.fromCharCode(value)}
+          </Text>
+        ))}
+      </ScrollView>
+      <View
+        style={{
+          position: 'absolute',
+          height: 30,
+          width: 30,
+          borderRadius: 15,
+          top: height / 2 - 15,
+          left: width - 40,
+          backgroundColor: 'red',
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
